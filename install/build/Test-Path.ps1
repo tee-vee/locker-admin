b51bbc07819c8c14649dc2346dfa5e9a2ffa47bb
Derek Yuen <tee-vee@users.noreply.github.com>
2017-01-01 02:13:01 +0800

    Delete Test-Path.ps1

--- install/build/Test-Path.ps1
+++
@@ -1,3 +0,0 @@
-$FileCheck = get-content C:\Users\XXXXX\desktop\Bacth1_FileCheck.txt
-
-ForEach ($File in $FileCheck) {if (test-path $File) {"$file exists">>C:\temp\Batch1_exists.log} else {"$file does not exist">>C:\temp\Batch1_doesnotexist.log}}
