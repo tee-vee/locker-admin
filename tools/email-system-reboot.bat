44cb529453032b9e18d065b6cb38222379a6a12a
LockerLife Locker Team <locker-admin@lockerlife.hk>
2017-01-04 05:34:34 +0800

    first batch of bulk upload

---
+++ tools/email-system-reboot.bat
@@ -0,0 +1,13 @@
+@echo off
+REM 
+REM Derek Yuen <derek.yuen@locision.com>
+REM 2016-11-06
+REM 
+REM email-system-reboot.bat
+REM
+REM NOTE: debug -vvv -info 
+
+rem set location=%1
+
+
+mailsend1.19.exe -t pi-admin@locision.com -f locker-status@locision.com -name "locker-status" -rp pi-admin@locision.com -rt pi-admin@locision.com -ssl -port 465 -auth -smtp hwsmtp.exmail.qq.com -domain locision.com -sub "test-locker system rebooted" -M "test-locker rebooted on %DATE% at %TIME%" -user pi-admin@locision.com -pass Locision1707 -q
