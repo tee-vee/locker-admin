89e165f072999e269678e0805a55c414b02b501d
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-11 16:58:04 -0500

    to resolve a merge conflict

---
+++ install/build/start-deploy.ps1
@@ -0,0 +1,10 @@
+Update-ExecutionPolicy Unrestricted
+cinst dotnet4.6.2
+cinst Boxstarter.Common
+cinst boxstarter.WinConfig
+cinst Boxstarter.Chocolatey
+cinst chocolatey
+cinst ie11
+cinst vim
+cinst gow -f
+cinst curl -f
