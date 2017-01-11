575ea0370249fd783ea6b0a41a4fac5ff343660f
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-11 17:35:47 -0500

    implemented -> stamp all downloaded file with remote-file-timestamp; added note to implement check via Last-Modified / and only download if modified is newer than file timestamp

--- install/build/update-locker-libs.bat
+++ install/build/update-locker-libs.bat
@@ -7,7 +7,7 @@
 REM ## setlocal enableDelayedExpansion
 setlocal
 
-set CURLOPTS=-Ss
+set CURLOPTS=-RSs
 set JQOPTS='.[].url'
 set XARGSOPTS=-P %NUMBER_OF_PROCESSORS%
 
@@ -16,10 +16,16 @@ set LOCKERCLOUDLIBPATH=/dev/lockers/libs
 
 set LOCKERLIBS=D:\locker-libs
 set LIBLIST=locker-libs-list.txt
+set LIBTIMESTAMP=locker-libs-timestamps.txt
 
 :: locate locker-libs first
 :: send output to locker-lib
 curl %CURLOPTS% --url https://%LOCKERCLOUDHOST%%LOCKERCLOUDLIBPATH% | jq %JQOPTS% > %LOCKERLIBS%\%LIBLIST%
+
+:: create timestamps file
+:: fetch Last-Modified header for specific file; only donwload if-modified
+REM ## cat %LIBLIST% | xargs %XARGSOPTS% -n 1 curl -LR
+
 
 :: download
 REM ## (e.g. cat or type %LIBLIST% | xargs -n 1 curl -LO )
