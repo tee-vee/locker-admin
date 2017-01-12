52353ea56ad6a6bb639f47826e442adcd8e335f2
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-12 05:00:31 -0500

    initial add to repo

---
+++ install/build/get-sitename.ps1
@@ -0,0 +1,20 @@
+function CustomInputBox([string] $title, [string] $message, [string] $defaultText) 
+{
+$inputObject = new-object -comobject MSScriptControl.ScriptControl
+$inputObject.language = "vbscript" 
+$inputObject.addcode("function getInput() getInput = inputbox(`"$message`",`"$title`" , `"$defaultText`") end function" ) 
+$_userInput = $inputObject.eval("getInput") 
+
+return $_userInput
+}
+
+
+$userInput = CustomInputBox "User Name" "Please enter your name." ""
+if ( $userInput -ne $null ) 
+{
+ echo "Input was [$userInput]"
+}
+else
+{
+ echo "User cancelled the form!"
+}
