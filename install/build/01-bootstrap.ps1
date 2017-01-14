d58f9ddb7d53c906d4e1129f965832fc3e9b749e
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-14 10:31:59 -0500

    0-windows-services.txt

---
+++ install/build/01-bootstrap.ps1
@@ -0,0 +1,9 @@
+# Derek Yuen <derekyuen@lockerlife.hk>
+# January 2017
+#
+# This is a placeholder file. Please refer to:
+#   PRODUCTION -> locker-admin GitHub Gist
+#   DEV -> Use your own Gist
+#  
+
+http://boxstarter.org/package/url?https://gist.githubusercontent.com/locker-admin/1b5e226d8f756411bbe2997b34099733/raw/53de96f0643cc9c3606726720a51db231434cebe/01-bootstrap.ps1
---
+++ install/build/02-bootstrap.ps1
@@ -0,0 +1,8 @@
+# Derek Yuen <derekyuen@lockerlife.hk>
+# January 2017
+#
+# This is a placeholder file. Please refer to:
+#   PRODUCTION -> locker-admin GitHub Gist
+#   DEV -> Use your own Gist
+#  
+
