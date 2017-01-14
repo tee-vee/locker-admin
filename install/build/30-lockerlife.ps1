ccb1c5b7125fc2e477841b98dabb4dd66b62ba3b
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-14 16:29:38 -0500

    moving off gist

--- install/build/30-lockerlife.ps1
+++ install/build/30-lockerlife.ps1
@@ -1,26 +1,59 @@
-:: Derek Yuen <derekyuen@locision.com>
-:: 
-:: December 2016
+# Derek Yuen <derekyuen@locision.com>
+# 
+# December 2016
 
 @echo off
 
 echo ====================================================
-echo          Start Locker Build Process - STAGE 1
+echo          Start Locker Build Process - STAGE 3
 echo ====================================================
-:: build.cmd debug -- build debug version.
-title "LOCKER DEPLOYMENT - STAGE 1"
+# build.cmd debug -- build debug version.
+title "LOCKER DEPLOYMENT - STAGE 3"
 
-:: [] ADD USER
-ECHO "%~n0 ADD KIOSK USER"
-REM %TOOLS%\hstart.exe /elevate /uac add-kiosk-user.bat
-start /wait net localgroup kiosk-group /add
-start /wait net user /add kiosk locision123 /active:yes /comment:"kiosk" /fullname:"kiosk" /passwordchg:no
-start /wait net localgroup "kiosk-group" "kiosk" /add
+# add user
+ECHO "ADD KIOSK USER"
+## %TOOLS%\hstart.exe /elevate /uac add-kiosk-user.bat
+& net localgroup kiosk-group /add
+& net user /add kiosk locision123 /active:yes /comment:"kiosk" /fullname:"kiosk" /passwordchg:no
+& net localgroup "kiosk-group" "kiosk" /add
 
 
-:: [] auto create user profile (super quick, super dirty!)
+# [] auto create user profile (super quick, super dirty!)
 ECHO "%~n0 Create kiosk user profile"
 psexec -accepteula -nobanner -u kiosk -p locision123 cmd /c dir
 psexec -accepteula -nobanner -u kiosk -p locision123 cmd /c dir
 
-:: SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+# psexec -u kiosk to use bginfo to change background to black
+
+# SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+#get locker-libs
+
+# set autologon to kiosk user
+
+# symlink \Users\kiosk\...\startup items\run.bat->dropbox\locker-shared\production\run.bat 
+# symlink D:\run.bat->dropbox\locker-shared\production\run.bat 
+
+# create finish-locker-setup.ps1 on kiosk\desktop, reboot
+
+#$WUSettings = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
+#$WUSettings
+#$WUSettings = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
+#
+#REM
+#REM NotificationLevel  :
+#REM     0 = Not configured;
+#REM     1 = Disabled;
+#REM     2 = Notify before download;
+#REM     3 = Notify before installation;
+#REM     4 = Scheduled installation;
+#REM
+#
+#$WUSettings.NotificationLevel=1
+#$WUSettings.save()
+#
+#REM [] DISABLE BINGSEARCH
+#Disable-BingSearch
+#Disable-GameBarTips
+#Update-Help
+
