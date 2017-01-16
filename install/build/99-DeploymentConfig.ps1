660d05890e988e8fef8d1fdc57eba0599931020a
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-16 12:01:26 -0500

    major refactoring/reordering

--- install/build/99-DeploymentConfig.ps1
+++ install/build/99-DeploymentConfig.ps1
@@ -1,1 +1,26 @@
 # Derek Yuen <derekyuen@lockerlife.hk>
+
+
+# Allow unattended reboots
+$Boxstarter.RebootOk=$true
+$Boxstarter.NoPassword=$false
+$Boxstarter.AutoLogin=$true
+
+#Install-ChocolateyEnvironmentVariable 'JAVA_HOME' 'path\to\jre' 'Machine'
+Install-ChocolateyEnvironmentVariable "baseurl" "http://lockerlife.hk/deploy"
+Install-ChocolateyEnvironmentVariable -variableName "JAVA_HOME" -variableValue "d:\java\jdk\bin" -variableType "Machine"
+Install-ChocolateyEnvironmentVariable "local" "C:\local"
+
+# Windows Explorer Settings
+Set-WindowsExplorerOptions -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -DisableShowRecentFilesInQuickAccess -DisableShowFrequentFoldersInQuickAccess
+
+# Small taskbar
+Set-TaskbarOptions -Size Small -Lock -Combine Full -Dock Bottom
+
+#Disable-MicrosoftUpdate
+#Disable-UAC
+#Update-ExecutionPolicy Unrestricted
+
+
+# Small taskbar
+Set-TaskbarOptions -Size Small -Lock -Combine Full -Dock Bottom
