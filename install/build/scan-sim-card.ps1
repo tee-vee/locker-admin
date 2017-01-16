4e297b9554edccf441affeaee7902512d45fa91b
Derek Yuen <derekyuen@lockerlife.hk>
2017-01-16 12:01:31 -0500

    major refactoring/reordering

--- install/build/scan-sim-card.ps1
+++ install/build/scan-sim-card.ps1
@@ -1,35 +1,19 @@
-# —————————————————————————————————
-
-# DotNetInputBoxV2.ps1
-
-# ed wilson, msft, 10/9/2008
-
+# Derek Yuen <derekyuen@lockerlife.hk>
+# December 2016
 #
-
 # uses the .Net framework interaction class with the inputbox method to
-
 # create an inputbox using the dot net framework classes. the microsoft.visualbasic
-
 # namespace is not loaded by default
-
-#
-
-# —————————————————————————————————
 
 #Requires -version 2.0
 
-$prompt = “Enter your command”
+$title = "LockerLife Deployment"
+$caption = "Scan SIM Card"
+$default = ""
 
-$title = “.net example”
-
-$default = “default value”
-
- 
-
-Add-Type  -AssemblyName microsoft.visualbasic
-
-$return = [Microsoft.VisualBasic.interaction]::inputbox($prompt,$title,$default)
-
-$return
+## https://msdn.microsoft.com/en-us/library/12xc5368%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
+Add-Type  -AssemblyName Microsoft.VisualBasic
+$env:iccid = [Microsoft.VisualBasic.interaction]::inputbox($caption,$title,"$env:iccid")
+$iccid
 
 
