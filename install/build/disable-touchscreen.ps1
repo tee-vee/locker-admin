0c1dabb6c9212eed8e9f13ab21796ff986a96d29
Derek Yuen <derekyuen@lockerlife.hk>
2017-02-07 01:41:05 -0500

    stability improvements and formalization

--- install/build/disable-touchscreen.ps1
+++ install/build/disable-touchscreen.ps1
@@ -1,3 +1,27 @@
+# Derek Yuen <derekyuen@lockerlife.hk>
+
+
+$ErrorActionPreference = "Continue"
+
+# Verify Running as Admin
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	1..5 | % { Write-Host }
+	exit
+}
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("$env:deployurl/bin/devcon.exe","$local\bin\devcon.exe")
+
+Write-Host "LockerLife Deployment Team - Tools"
 Write-Host "Disabling Touch Screen"
-devcon status "USB\VID_0EEF&PID_C000"
-devcon disable "USB\VID_0EEF&PID_C000"
+
+Disable-UAC
+
+C:\local\bin\devcon status "USB\VID_0EEF&PID_C000"
+C:\local\bin\devcon disable "USB\VID_0EEF&PID_C000"
+
+# cleanup
+Enable-UAC
--- install/build/enable-touchscreen.ps1
+++ install/build/enable-touchscreen.ps1
@@ -1,2 +1,26 @@
-Write-Host "Disabling Touch Screen"
-devcon enable "USB\VID_0EEF&PID_C000"
+# Derek Yuen <derekyuen@lockerlife.hk>
+
+
+$ErrorActionPreference = "Continue"
+
+# Verify Running as Admin
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+If (!( $isAdmin )) {
+	Write-Host "-- Restarting as Administrator" -ForegroundColor Cyan ; Sleep -Seconds 1
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	1..5 | % { Write-Host }
+	exit
+}
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("$env:deployurl/bin/devcon.exe","$local\bin\devcon.exe")
+
+Write-Host "LockerLife Deployment Team - Tools"
+Write-Host "Enabling Touch Screen"
+
+Disable-UAC
+
+C:\local\bin\devcon status "USB\VID_0EEF&PID_C000"
+C:\local\bin\devcon enable "USB\VID_0EEF&PID_C000"
+
+Enable-UAC
