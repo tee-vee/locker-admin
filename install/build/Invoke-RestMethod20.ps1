55a94b9409a16d71c7e28d9b4ca5e60eea8d98ac
Derek Yuen <derekyuen@lockerlife.hk>
2017-03-17 11:18:39 -0400

    cleanup

--- install/build/Invoke-RestMethod20.ps1
+++ install/build/Invoke-RestMethod20.ps1
@@ -21,96 +21,96 @@ Add-Type -TypeDefinition @"
 "@
 
 Function ConvertTo-Json20 {
-  [CmdletBinding()]
-  Param(
-    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
-    [object]$item
-  )
-  PROCESS {
-    Add-Type -assembly system.web.extensions
-    $ps_js=new-object system.web.script.serialization.javascriptSerializer
-    return $ps_js.Serialize($item)
-  }
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+        [object]$item
+    )
+    PROCESS {
+        Add-Type -assembly system.web.extensions
+        $ps_js=new-object system.web.script.serialization.javascriptSerializer
+        return $ps_js.Serialize($item)
+    }
 }
 
 Function ConvertFrom-Json20 {
-  [CmdletBinding()]
-  Param (
-    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
-    [object]$item
-  )
-  PROCESS {
-    Add-Type -assembly system.web.extensions
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)]
+        [object]$item
+    )
+    PROCESS {
+        Add-Type -assembly system.web.extensions
         $ps_js=new-object system.web.script.serialization.javascriptSerializer
         return $ps_js.DeserializeObject($item)
-      }
     }
+}
 
 Function Invoke-RestMethod20 {
-  param (
-    [Parameter(Mandatory)]
-    [RestMethod]
-    $Method,
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [System.String]
-    $Uri,
-    [Parameter(Mandatory=$false)]
-    [object]
-    $Body,
-    [Parameter(Mandatory=$false)]
-    [System.string]
-    $User,
-    [Parameter(Mandatory=$false)]
-    [System.String]
-    $Password,
-    [Parameter(Mandatory=$false)]
-    [System.String]
-    $UserAgent,
-    [Parameter(Mandatory=$false)]
-    [switch]
-    $asJson,
-    [System.String]
-    $ContentType = 'application/json',
-    [Parameter(Mandatory=$false)]
-    [object]
-    $Headers
-  )
-  $webclient = New-Object System.Net.WebClient
-  $webclient.Headers.add("ContentType", $ContentType)
+    param (
+        [Parameter(Mandatory)]
+        [RestMethod]
+        $Method,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Uri,
+        [Parameter(Mandatory=$false)]
+        [object]
+        $Body,
+        [Parameter(Mandatory=$false)]
+        [System.string]
+        $User,
+        [Parameter(Mandatory=$false)]
+        [System.String]
+        $Password,
+        [Parameter(Mandatory=$false)]
+        [System.String]
+        $UserAgent,
+        [Parameter(Mandatory=$false)]
+        [switch]
+        $asJson,
+        [System.String]
+        $ContentType = 'application/json',
+        [Parameter(Mandatory=$false)]
+        [object]
+        $Headers
+    )
+    $webclient = New-Object System.Net.WebClient
+    $webclient.Headers.add("ContentType", $ContentType)
 
-  # Additional or Extra Headers
-  if ($Headers -ne $null) {
-    $webclient.Headers.Add($Headers)
-  }
+    # Additional or Extra Headers
+    if ($Headers -ne $null) {
+        $webclient.Headers.Add($Headers)
+    }
 
-  # Autentication
-  if ($user -ne $null -and $Password -ne $null) {
-    $creds = New-Object System.Net.NetworkCredential($User,$Password);
-    $webclient.Credentials = $creds
-  }
+    # Autentication
+    if ($user -ne $null -and $Password -ne $null) {
+        $creds = New-Object System.Net.NetworkCredential($User,$Password);
+        $webclient.Credentials = $creds
+    }
 
-  # User Agent
-  if ($UserAgent -ne $null) {
-    $webclient.Headers.Add("user-agent", $UserAgent)
-  }
+    # User Agent
+    if ($UserAgent -ne $null) {
+        $webclient.Headers.Add("user-agent", $UserAgent)
+    }
 
-  # Get Response
-  try {
-    if ($Method -eq "GET") {
-      $response = $webClient.DownloadString($Uri)
-      } #if
-      elseif ($Method -eq "PUT" -or $Method -eq "POST") {
-        $response = $webClient.UploadString($Uri,$Method,$Body)
-      }
+    # Get Response
+    try {
+        if ($Method -eq "GET") {
+            $response = $webClient.DownloadString($Uri)
+        } #if
+        elseif ($Method -eq "PUT" -or $Method -eq "POST") {
+            $response = $webClient.UploadString($Uri,$Method,$Body)
+        }
     }
 
     catch {
-      Write-host $_.Exception
+        Write-host $_.Exception
     }
     # Return Response
     if ($asJson) {
-      return $response
+        return $response
     }
     return $response | ConvertFrom-Json20Pipe
-  }
+}
