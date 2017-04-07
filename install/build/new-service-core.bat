1347ab468a45d87f76f92878af7fad9a5243cc05
Derek Yuen <derekyuen@lockerlife.hk>
2017-04-07 07:46:03 -0400

    cleanup and or minor formatting changes

--- install/build/new-service-core.bat
+++ install/build/new-service-core.bat
@@ -1,21 +1,21 @@
-@echo off
-c:\local\bin\NSSM.exe install core D:\java\jre\bin\java.exe
-c:\local\bin\NSSM.exe set core DisplayName core
-c:\local\bin\NSSM.exe set core Description LockerLife
-c:\local\bin\NSSM.exe set core Application D:\java\jre\bin\java.exe
-c:\local\bin\NSSM.exe set core AppParameters -Dconfig=D:\locker-configuration.properties -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -jar d:\core.jar
-c:\local\bin\NSSM.exe set core AppDirectory D:\java\jre\bin
-c:\local\bin\NSSM.exe set core AppEnvironmentExtra "JAVA_HOME=D:\java\jre"
-c:\local\bin\NSSM.exe set core Start SERVICE_AUTO_START
-c:\local\bin\NSSM.exe set core AppStdout e:\logs\core-service.log
-c:\local\bin\NSSM.exe set core AppStderr e:\logs\core-service.err
-c:\local\bin\NSSM.exe set core AppRotateFiles 1
-c:\local\bin\NSSM.exe set core AppRotateOnline 0
-c:\local\bin\NSSM.exe set core AppRotateSeconds 86400
+@ECHO OFF
+NSSM.exe install core D:\java\jre\bin\java.exe
+NSSM.exe set core DisplayName core
+NSSM.exe set core Description LockerLife
+NSSM.exe set core Application D:\java\jre\bin\java.exe
+NSSM.exe set core AppParameters -Dconfig=D:\locker-configuration.properties -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -jar d:\core.jar
+NSSM.exe set core AppDirectory D:\java\jre\bin
+NSSM.exe set core AppEnvironmentExtra "JAVA_HOME=D:\java\jre"
+NSSM.exe set core Start SERVICE_AUTO_START
+NSSM.exe set core AppStdout e:\logs\core-service.log
+NSSM.exe set core AppStderr e:\logs\core-service.err
+NSSM.exe set core AppRotateFiles 1
+NSSM.exe set core AppRotateOnline 0
+NSSM.exe set core AppRotateSeconds 86400
 REM Replace stdout and stderr files
-c:\local\bin\NSSM.exe set core AppStdoutCreationDisposition 4
-c:\local\bin\NSSM.exe set core AppStderrCreationDisposition 4
+NSSM.exe set core AppStdoutCreationDisposition 4
+NSSM.exe set core AppStderrCreationDisposition 4
 REM Disable WM_CLOSE, WM_QUIT in the Shutdown options. Without it, NSSM can't stop core properly
-c:\local\bin\NSSM.exe set core AppStopMethodSkip 0
+NSSM.exe set core AppStopMethodSkip 0
 REM Let's start core. I assume a correct configuration is already in place
 NET start core
--- install/build/new-service-datacollection.bat
+++ install/build/new-service-datacollection.bat
@@ -1,21 +1,21 @@
 @ECHO OFF
-c:\local\bin\NSSM.exe install data-collection D:\java\jre\bin\java.exe
-c:\local\bin\NSSM.exe set data-collection DisplayName data-collection
-c:\local\bin\NSSM.exe set data-collection Description LockerLife
-c:\local\bin\NSSM.exe set data-collection Application D:\java\jre\bin\java.exe
-c:\local\bin\NSSM.exe set data-collection AppParameters -Dconfig=D:\locker-configuration.properties -jar D:\data-collection.jar
-c:\local\bin\NSSM.exe set data-collection AppDirectory D:\java\jre\bin
-c:\local\bin\NSSM.exe set data-collection AppEnvironmentExtra "JAVA_HOME=D:\Java\jre"
-c:\local\bin\NSSM.exe set data-collection Start SERVICE_DELAYED_AUTO_START
-c:\local\bin\NSSM.exe set data-collection AppStdout e:\logs\data-collection-service.log
-c:\local\bin\NSSM.exe set data-collection AppStderr e:\logs\data-collection-service.err
-c:\local\bin\NSSM.exe set data-collection AppRotateFiles 1
-c:\local\bin\NSSM.exe set data-collection AppRotateOnline 0
-c:\local\bin\NSSM.exe set data-collection AppRotateSeconds 86400
+NSSM.exe install data-collection D:\java\jre\bin\java.exe
+NSSM.exe set data-collection DisplayName data-collection
+NSSM.exe set data-collection Description LockerLife
+NSSM.exe set data-collection Application D:\java\jre\bin\java.exe
+NSSM.exe set data-collection AppParameters -Dconfig=D:\locker-configuration.properties -jar D:\data-collection.jar
+NSSM.exe set data-collection AppDirectory D:\java\jre\bin
+NSSM.exe set data-collection AppEnvironmentExtra "JAVA_HOME=D:\Java\jre"
+NSSM.exe set data-collection Start SERVICE_DELAYED_AUTO_START
+NSSM.exe set data-collection AppStdout e:\logs\data-collection-service.log
+NSSM.exe set data-collection AppStderr e:\logs\data-collection-service.err
+NSSM.exe set data-collection AppRotateFiles 1
+NSSM.exe set data-collection AppRotateOnline 0
+NSSM.exe set data-collection AppRotateSeconds 86400
 REM Replace stdout and stderr files
-c:\local\bin\NSSM.exe set data-collection AppStdoutCreationDisposition 4
-c:\local\bin\NSSM.exe set data-collection AppStderrCreationDisposition 4
+NSSM.exe set data-collection AppStdoutCreationDisposition 4
+NSSM.exe set data-collection AppStderrCreationDisposition 4
 REM Disable WM_CLOSE, WM_QUIT in the Shutdown options. Without it, NSSM can't stop data-collection properly
-c:\local\bin\NSSM.exe set data-collection AppStopMethodSkip 0
+NSSM.exe set data-collection AppStopMethodSkip 0
 REM Let's start data-collection. I assume a correct configuration is already in place
 NET START data-collection
