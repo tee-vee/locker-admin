89c143c6f3793a74dcd683ada8453d9643f9906a
Derek Yuen <derekyuen@lockerlife.hk>
2017-04-08 16:04:56 -0400

    identify user and do different tasks

--- install/run.bat
+++ install/run.bat
@@ -1,20 +1,13 @@
 @echo off
 REM
 REM  Gilbert Zhong, Francis Kwok, Derek Yuen
-
 REM  locker-console startup
 
 REM
-
 REM  NOTES
-
 REM    - ALL locker-console computer production stuff in D: drive
-
 REM    - ALL production program have no version number
-
 REM
-
-
 
 REM  [1] START SCANNER
 
@@ -36,11 +29,24 @@ REM  [4] START DATA-COLLECTION-ALL
 rem start /min java -jar data-collection-all-1.0.jar
 rem timeout 5 > NUL
 
-REM 2016-11-25 dky - as requested by Francis
+if "%username%" == "kiosk" goto run
+
+goto :finished
+
+:run
+REM REG.EXE ADD HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer /V NoStartMenuMyMusic /t DWORD /d 1 /F
+
 d:
-cd d:\Locker-Console
-start LockerLife.exe
-timeout 1 > NUL
 cd d:\locker-slider
 start LockerLife_TV.exe
 
+c:\windows\System32\timeout.exe /T 5 > NUL
+
+REM 2016-11-25 dky - as requested by Francis
+cd d:\Locker-Console
+start LockerLife.exe
+c:\windows\System32\timeout.exe /T 1 > NUL
+
+
+:finished
+exit
